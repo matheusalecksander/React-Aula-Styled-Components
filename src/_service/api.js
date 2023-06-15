48f8bf4b541c35a58c4615f574cd43ab11e0669f
pedroso <dev10@lwtecnologia.com.br>
2023-06-15 20:50:25 -0300

    Aula 5: url com https

--- src/_service/api.js
+++ src/_service/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://localhost:5000",
-  baseURL: "http://webapp353621.ip-45-79-142-173.cloudezapp.io/api",
+  baseURL: "https://webapp353621.ip-45-79-142-173.cloudezapp.io/api",
   // baseURL: "localhost:5001/api",
 });
 
