9e4673fd43ab5b5e680490d0626d5a7754f5ea37
pedroso <dev10@lwtecnologia.com.br>
2023-08-01 17:42:48 -0300

    Alterando imagens e alterando carrossel

--- src/componentes/CarouselAnuncios.jsx
+++ src/componentes/CarouselAnuncios.jsx
@@ -1,35 +1,38 @@
 import { Carousel } from "react-bootstrap";
+import img1 from "./../assets/images/banner_img (1).jpg";
+import img2 from "./../assets/images/banner_img (2).jpg";
+import img3 from "./../assets/images/banner_img (3).jpg";
+import img4 from "./../assets/images/banner_img (4).jpg";
+import img5 from "./../assets/images/banner_img (5).jpg";
+import img6 from "./../assets/images/banner_img (6).jpg";
 
 function CarouselAnuncios() {
   return (
     <Carousel>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://via.placeholder.com/800x200.png?text=Anuncio+1"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
+        <img className="d-block w-100" src={img1} alt="First slide" />
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://via.placeholder.com/800x200.png?text=Anuncio+2"
-          alt="Second slide"
-        />
+        <img className="d-block w-100" src={img2} alt="First slide" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={img3} alt="Second slide" />
+      </Carousel.Item>
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
+      <Carousel.Item>
+        <img className="d-block w-100" src={img4} alt="First slide" />
       </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={img5} alt="First slide" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={img6} alt="First slide" />
+      </Carousel.Item>
+
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://via.placeholder.com/800x200.png?text=Anuncio+3"
+          src="https://via.placeholder.com/800x200.png?text=Anuncio+8"
           alt="Third slide"
         />
 
